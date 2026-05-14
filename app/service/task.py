5230c37f69ca8d9c36aae6ff8f6d7a60f99ad4ab
Sanzhar <goodchildfds@gmail.com>
2026-05-14 22:55:45 +0500

    declare database.py and connect database sqlite

--- app/service/task.py
+++ app/service/task.py
@@ -1,6 +1,7 @@
 from fastapi import HTTPException
 
 from app.schemas import CreateTaskRequest, TaskResponse
+from app.repository import task as task_repository
 
 def create_task(task: CreateTaskRequest):
     # if task_repository Is_task exist return httpexception 400 alredy exist
